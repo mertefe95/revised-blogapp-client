0333b8d74cd1e13b542a24b84a97d94270b30fef
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-12-19 01:59:04 +0300

    uncommitted files

---
+++ src/components/blog-template/Header.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import AuthOptions from "../utils/AuthOptions"
+import { Container } from "@material-ui/core";
+import  { useHistory} from "react-router-dom";
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+  toolbarSecondary: {
+    justifyContent: 'space-between',
+    overflowX: 'auto',
+  },
+  toolbarLink: {
+    padding: theme.spacing(1),
+    flexShrink: 0,
+  },
+}));
+
+const sections = [
+  { title: 'Technology', url: '#' },
+  { title: 'Design', url: '#' },
+  { title: 'Culture', url: '#' },
+  { title: 'Business', url: '#' },
+  { title: 'Politics', url: '#' },
+  { title: 'Opinion', url: '#' },
+  { title: 'Science', url: '#' },
+  { title: 'Health', url: '#' },
+  { title: 'Style', url: '#' },
+  { title: 'Travel', url: '#' },
+];
+
+
+
+
+export default function Header(props) {
+  const classes = useStyles();
+  const {  title } = props;
+  const history = useHistory();
+  const click = () => history.push("/") 
+
+  return (
+    <React.Fragment>
+    <Container maxWidth="lg">
+      <Toolbar className={classes.toolbar}>
+        <Button size="small" onClick={click}>View Posts</Button>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          className={classes.toolbarTitle}
+        >
+          {title}
+        </Typography>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <AuthOptions />
+      </Toolbar>
+      <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
+        {sections.map((section) => (
+          <Link
+            color="inherit"
+            noWrap
+            key={section.title}
+            variant="body2"
+            href={section.url}
+            className={classes.toolbarLink}
+          >
+            {section.title}
+          </Link>
+        ))}
+      </Toolbar>
+      </Container>
+    </React.Fragment>
+  );
+}
---
+++ src/components/blog-template/Main.js
@@ -0,0 +1,71 @@
+
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Markdown from './Markdown';
+import React, { useContext, useState } from "react";
+import UserContext from ".././context/UserContext";
+import { Link } from "react-router-dom";
+import Axios from "axios";
+
+
+
+
+
+export default function Main(props) {
+  const { title, posts } = props;
+  const { userData } = useContext(UserContext); 
+  const [post, setPost] = useState([]);
+  const [error, setError] = useState();
+
+  const useStyles = makeStyles((theme) => ({
+    markdown: {
+      ...theme.typography.body2,
+      padding: theme.spacing(3, 0),
+    },
+  }));
+  
+  const deletePost = id => {
+    try {
+        Axios.delete(`https://blog-app-revised.herokuapp.com/posts/${id}`)
+            .then(res => alert(res.data))
+            setPost(post.filter(elem => elem._id !== id));
+        }   catch (err) {
+            err.response.data.msg && setError(err.response.data.msg);
+        }}
+
+  const classes = useStyles();
+
+
+  return (
+    <Grid item xs={12} md={8}>
+      <Typography variant="h6" gutterBottom>
+        {title}
+      </Typography>
+      <Divider />
+      {posts.map((post, key) => (
+        <div className="post-div" key={key}>
+          <h5 className="post-title"><Link className="title-link" to={{
+            pathname: `/post/${post._id}`
+          }}>{post.blogTitle}</Link></h5>
+          <p className="post-time-author">{post.createdAt} by <Link className="author-link" to="#">{post.authorName}</Link> </p>
+          <p className="post-text ">{post.blogText}</p>
+      
+          { userData.user && userData.user.id === post.userId ? (
+            <div className="post-icons-div">
+            <Link to={`/edit-post/${post._id}`}><i className="edit far fa-edit"></i></Link>
+            <Link className="bin-link"><button className="bin-button" onClick={() => deletePost(post._id)}><i className="bin far fa-trash-alt"></i></button></Link>
+            </div>
+        ) : (
+            <>
+
+            </>
+        )}
+    
+          </div>
+      ))}
+    </Grid>
+  );
+}
